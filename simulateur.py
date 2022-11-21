fb74d6fb82000bb45cab907313de04ffc53ee1d6
ClemMoreau <clement.moreau92@gmail.com>
2022-11-21 16:39:50 +0100

    ajout event arrivée file 1

--- simulateur.py
+++ simulateur.py
@@ -1,1 +1,14 @@
-# TODO tous niquer
+# file d'attente station contrôle  
+Q1 = 0
+
+def ArriveFile1(Q1):
+    Q1 = Q1 + 1
+    if Q1 == 1:
+        Schedule("DepartFile1", 0.5)
+
+# fonction d'exécution des evenements
+def Schedule(event, delay):
+    global Time
+    Time = Time + delay
+    print("Time = ", Time, "Event = ", event)
+    
