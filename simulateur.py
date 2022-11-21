f9d5cd709affba9398711229da87e172fb858b3a
Robin <one.robin@outlook.fr>
2022-11-21 16:49:28 +0100

    Ajout ArrivéControle

--- simulateur.py
+++ simulateur.py
@@ -1,1 +1,10 @@
 # TODO tous niquer
+
+def AccesPosteControle():
+    # TODO Decrementer le nombre de client dans la files
+    global fileDatt
+    global postestatus
+    fileDatt = fileDatt - 1
+    postestatus = False
+    DepartPosteControle()
+
