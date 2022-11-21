c6449ee1aef74bc8c1105be6113727ef102a47c0
MILLAIRE Theo <theo.millaire@soprasteria.com>
2022-11-21 17:04:10 +0100

    Fonction de departPosteControl

--- simulateur.py
+++ simulateur.py
@@ -1,1 +1,12 @@
-# TODO tous niquer
+import numpy as np
+
+
+def departPosteControl():
+    global statusControl
+    statusControl = 0
+    if NombreBusFileControl > 0:
+        # Ici mettre date à l'heure précis
+        AccesControl()
+    if np.random.normal(0, 1) < 0.3:
+        # Ici mettre date à l'heure précis
+        AccesFileR()
