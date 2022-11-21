8f351e0c777b9d64fec60f6cd38b9a7a58e5e94a
ClemMoreau <clement.moreau92@gmail.com>
2022-11-21 16:54:38 +0100

    arrive file 2

--- simulateur.py
+++ simulateur.py
@@ -1,1 +1,13 @@
-# TODO tous niquer
+Q2 = 0
+B2 = False
+
+def ArriveFile2():
+    global Q2, B2
+    Q2 += 1
+    if not B2:
+        B2 = True
+        Shedule("AccesGuichet2", 0)
+    return Q2
+
+def Shedule(event, delay):
+    global Q2, B2
