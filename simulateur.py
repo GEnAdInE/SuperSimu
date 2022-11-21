e9459756cd9d272bfd842138ad72c4212be85634
ClemMoreau <clement.moreau92@gmail.com>
2022-11-21 17:17:20 +0100

    add class

--- simulateur.py
+++ simulateur.py
@@ -1,62 +1,32 @@
-
 import numpy as np
 
-
-Q2 = 0
-B2 = 0
-nbBusRepair = 0
-
-def ArriveFile2():
-    global Q2, B2, nbBusRepair
-    Q2 += 1
-    nbBusRepair += 1
-    if B2 < 2:
-        Shedule("AccesGuichet2", 0)
-    return Q2
+class Schedule:
     
-# file d'attente station contrôle  
-Q1 = 0
+    def __init__(self, duree):
+        self.duree_simulation = duree
+        self.schedule = []
 
-# État guichet contrôle
-B1 = False
-
-def ArriveFile1():
-    global Q1, B1
-    Q1 = Q1 + 1
-    if Q1 == 1:
-        Schedule("AccesGuichetC", 0)
-    return Q1
-
-# fonction d'exécution des evenements
-def Schedule(event, delay):
-    global Time
-    Time = Time + delay
-    print("Time = ", Time, "Event = ", event)
+    def arriveFileReparation(self):
+        self.Q2 += 1
+        self.nbBusRepair += 1
+        if self.B2 < 2:
+            self.schedule.append("AccesGuichetR", 0)
+            
+    def arriveFileControle(self):
+        self.Q1 = self.Q1 + 1
+        if self.Q1 == 1:
+            self.schedule.append("AccesGuichetC", 0)
+        
+    def accesPosteControle(self):
+        self.Q1 -= 1
+        self.B1 = False
+        self.schedule.append("DepartControle", "AJOUTER TEMPS")    
     
 
-def DepartPosteControl():
-    global statusControl
-    statusControl = 0
-    if NombreBusFileControl > 0:
-        # Ici mettre date à l'heure précis
-        AccesPosteControle()
-    if np.random.normal(0, 1) < 0.3:
-        # Ici mettre date à l'heure précis
-        AccesFileR()
-
-def AccesPosteControle():
-    global fileDattControle
-    global posteControlStatus
-    fileDattControle = fileDattControle - 1
-    posteControlStatus = True
-    DepartPosteControl() #TODO : truc de temps
-
-def AccesPosteReparation():
-    global fileDattRepairs
-    global posteReparation1Status
-    fileDattRepairs = fileDattRepairs - 1
-    posteReparation1Status = True
-    DepartPosteReparation1() #TODO : truc de temps
+    def AccesPosteReparation(self):
+        self.Q2 -= 1
+        self.B2 += 1
+        self.schedule.append("DepartReparation", "AJOUTER TEMPS")    
 
 
 def DebutSimu(dureeSimu):
