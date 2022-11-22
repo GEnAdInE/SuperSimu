52f1ce8a75cf780d8c67ffb228ba2a2b1e961281
ClemMoreau <clement.moreau92@gmail.com>
2022-11-22 10:34:45 +0100

    add last event

--- simulateur.py
+++ simulateur.py
@@ -1,20 +1,4 @@
-# dureesSimulation = [40, 80, 160, 240]
-
-'''for duree in dureesSimulation:
-    init(duree)
-
-    while echeancier:
-        # On recupere le prochain evenement
-        evenement = echeancier.pop(0)
-        dateSysteme = evenement[0]
-        # On execute l'evenement
-        evenement[1]()
-
-    print("Fin de la simulation apres " + str(duree) + " heures")'''
-
 import numpy.random as np
-
-
 class Echeancier(list):
     def __init__(self, *args, **kwargs):
         super(Echeancier, self).__init__(*args, **kwargs)
@@ -42,7 +26,21 @@ class Simulateur:
     def __init__(self, duree):
         self.duree_simulation = duree
         self.schedule = Echeancier()
-        self.DebutSimu()
+        
+
+        # on initialise les variables une première fois pour qu'on puisse y avoir accès après
+        # elle seront réinitialisées dans la méthode DebutSimu
+        self.nbBus = 0
+        self.nbBusRepair = 0
+        self.Q1 = 0
+        self.Q2 = 0
+        self.B1 = True
+        self.B2 = 0
+
+        self.AireQ1 = 0
+        self.AireQ2 = 0
+        self.AireB2 = 0
+
     
     # Arrivee d'un bus
     def arriveeBus(self):
@@ -57,7 +55,7 @@ class Simulateur:
     # arrivé bus dans la file de controle
     def arriveFileControle(self):
         self.Q1 = self.Q1 + 1
-        if self.Q1 == 1:
+        if self.B1 == False:
             self.schedule.append((self.accesPosteControle, self.heureSysteme))
         
     # accès guichet controle
@@ -70,7 +68,6 @@ class Simulateur:
         
         self.B1 = True
         if self.Q1 > 0:
-            # Ici mettre date à l'heure précis
             self.schedule.append((self.accesPosteControle, self.heureSysteme))
         if np.uniform(0, 1) < 0.3:
 
@@ -98,7 +95,6 @@ class Simulateur:
             self.schedule.append((self.accesPosteReparation, self.heureSysteme))
 
     def DebutSimu(self):
-        self.heureSysteme = 0
         self.nbBus = 0
         self.nbBusRepair = 0
         self.Q1 = 0
@@ -110,18 +106,56 @@ class Simulateur:
         self.AireQ2 = 0
         self.AireB2 = 0
         
-        self.schedule.append((self.arriveesBus, self.heureSysteme + np.exponential((4/3)) * 120)) # 120min = 2h
+        self.schedule.append((self.arriveeBus, self.heureSysteme + np.exponential((4/3)) * 120)) # 120min = 2h
         self.schedule.append((self.FinSimulation, self.duree_simulation))
 
     def FinSimulation(self):
 
-        # TODO : Vider l'échéancier
         # TODO : Changer variables
         # TODO : Ajouter variable pour nb d'heures de simulation
+
+        self.schedule.clear()
 
         print("Temps d'attention moyen avant contrôle : ", self.AireQ1 / self.nbBus)
         print("Temps d'attention moyen avant réparation : ", self.AireQ2 / self.nbBusRepair)
         print("Temps d'utilisation moyen du centre de réparation : ", self.AireB2 / (2 * self.duree))
 
-        
+    def MAJAires(self, nextDate):
+        self.AireQ1 += self.Q1 * (nextDate - self.heureSysteme)
+        self.AireQ2 += self.Q2 * (nextDate - self.heureSysteme)
+        self.AireB2 += self.B2 * (nextDate - self.heureSysteme)
 
+    def run(self):
+        # init heure système
+        self.heureSysteme = 0
+
+        # Ajout de l'évènement de début de simulation
+        self.schedule.append((self.DebutSimu, self.heureSysteme))
+
+        # Tant qu'il y a des evenements dans l'echeancier
+        while self.schedule != []:
+
+            # Extraction du prochain evenement
+            nextEvent = self.schedule.prochain()
+
+            # Mise à jour des aires 
+            self.MAJAires(nextEvent[1])
+
+            # Mise à jour de l'heure système
+            self.heureSysteme = nextEvent[1]
+
+            print("Heure système : ", self.heureSysteme)
+            print("Evenement : ", nextEvent[0])
+
+            # Execution de l'evenement
+            nextEvent[0]()
+
+        # Fin de la simulation
+
+if __name__ == '__main__': 
+    dureesSimulation = [40]#, 80, 160, 240]
+    for duree in dureesSimulation:
+        print("Simulation de durée : ", duree)
+        simulateur = Simulateur(duree)
+        simulateur.run()
+        print("Fin de la simulation : ", duree)
